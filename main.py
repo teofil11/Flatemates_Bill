b55761d3eef1ae501fc7b259d8411f477867335c
Teo <teodorescuteofil@gmail.com>
2023-09-13 21:25:29 +0300

    Generate pdf complete

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from fpdf import FPDF
+import webbrowser
+
 class Bill:
     """
     Object that contains data about a bill,
@@ -31,8 +34,32 @@ class PdfReport:
     and the period
     """
 
-    def __init__(self,filemate):
-        self.filename = filemate
+    def __init__(self,filename):
+        self.filename = filename
 
-    def generate(self,flatmate1,flatemate2, bill):
-        pass
+    def generate(self,flatmate1,flatmate2, bill):
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        #Add icon
+        pdf.image('Flatmates_bill/bill_icon.png', w=30, h=30)
+
+        #Inset Title
+        pdf.set_font(family='Arial', style='B', size=24)
+        pdf.cell(w=0, h=80, txt='Flatmate Report', border=1, align='C', ln=1)
+
+        #Insert Period values
+        pdf.set_font(family='Times', style='B', size=20)
+        pdf.cell(w=0, h=40,txt=bill.period,border=1, ln=1)
+
+        #Insert name and due amount of the first flatmate
+        pdf.set_font(family='Times', style='B', size=12)
+        pdf.cell(w=100, h=40, txt='Name:' + flatmate1.name, border=1,)
+        pdf.cell(w=0, h=40, txt='Due:' + str(round(flatmate1.pays(bill, flatmate2), 2)), border=1, ln=1)
+
+        #Insert name and due amount of the second flatmate
+        pdf.cell(w=100, h=40, txt='Name:' + flatmate2.name, border=1)
+        pdf.cell(w=0, h=40, txt='Due:' + str(round(flatmate2.pays(bill, flatmate1), 2)), border=1, ln=1)
+
+        pdf.output(self.filename)
+        webbrowser.open(self.filename)
